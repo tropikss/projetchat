9777518f0307ea6f41d63b74cd3fca1d81a53a25
mathis.friess <mathis.friess@igtp3-pc15.isim.intra>
2024-01-22 20:05:51 +0100

    ghghg

--- projet-chat-backup/driver_joystick.py
+++ projet-chat-backup/driver_joystick.py
@@ -4,7 +4,7 @@ from time import *
 
 # retourne la position
 # 0 = bas, 1 = droite, 2 = haut, 3 = gauche, 4 = centre
-def read5way(adresse=0x03)
+def read5way(adresse=0x03):
 	sleep(0.1)
 	lect = read_i2c_block(adresse)
 	if lect[3] == 128:
